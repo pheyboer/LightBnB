b05f43b73689b0a5033cfbde287ac68b6f3d7909
Peter Heyboer <heyboer.peter@gmail.com>
2024-12-16 13:38:00 -0500

    refactoring getAllReservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -84,6 +84,7 @@ const getUserWithId = (id) => {
 //   return Promise.resolve(user);
 // };
 
+// Refactored code
 const addUser = (user) => {
   return pool
     .query(
@@ -108,8 +109,32 @@ const addUser = (user) => {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+// Original Code
+// const getAllReservations = function (guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
+
+// Refactored Code
+const getAllReservations = (guest_id, limit = 10) => {
+  return pool
+    .query(
+      `SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+        FROM reservations
+        JOIN properties ON reservations.property_id = properties.id
+        JOIN property_reviews ON properties.id = property_reviews.property_id
+        WHERE reservations.guest_id = $1
+        GROUP BY properties.id, reservations.id
+        ORDER BY reservations.start_date
+        LIMIT $2;`,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      // console.log(result.rows);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
